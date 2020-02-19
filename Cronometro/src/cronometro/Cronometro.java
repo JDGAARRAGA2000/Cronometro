9b7a5387825fa52224c476f32413ffb4baacef86
JDGAARRAGA2000 <61156440+JDGAARRAGA2000@users.noreply.github.com>
2020-02-19 15:52:12 -0500

    Add files via upload

---
+++ Cronometro/src/cronometro/Cronometro.java
@@ -0,0 +1,18 @@
+
+package cronometro;
+
+
+
+public class Cronometro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       FrmCronometro frmCronometro=new FrmCronometro();
+       //frmAdivina.show();
+       frmCronometro.setVisible(true);
+    }
+   }
+    
